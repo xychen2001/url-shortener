297382266941a3193cd5690608832d733e81ec6f
xychen2001 <157802095+xychen2001@users.noreply.github.com.>
2026-05-14 01:27:12 +0800

    add react router dom and refactor to have routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 const rootElement = document.getElementById('root')
 if (!rootElement) {
@@ -10,6 +11,8 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
